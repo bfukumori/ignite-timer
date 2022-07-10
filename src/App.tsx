9c427aba485a8279dfbdd89015ae26b983e485f9
Bruno Fukumori <brunofukumori@gmail.com>
2022-07-10 01:30:53 -0300

    01-estrutura da aplicação

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
