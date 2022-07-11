94b7f06fc0fb6e072793d629b7df0911ec6231c9
Bruno Fukumori <brunofukumori@gmail.com>
2022-07-10 22:22:43 -0300

    fix to history refresh page 404 error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { CyclesContextProvider } from './contexts/CyclesContext'
 import { Router } from './Router'
@@ -8,11 +8,11 @@ import { defaultTheme } from './styles/themes/default'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
+      <HashRouter>
         <CyclesContextProvider>
           <Router />
         </CyclesContextProvider>
-      </BrowserRouter>
+      </HashRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
