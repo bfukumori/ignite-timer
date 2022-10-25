d82085cc599e4594239c36fa091df7989a82ee24
Bruno Fukumori <brunofukumori@gmail.com>
2022-10-25 01:06:07 -0300

    update: dependencies and fix input step

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -31,7 +31,7 @@ export function NewCycleForm() {
         id="minutesAmount"
         placeholder="00"
         step={5}
-        min={1}
+        min={0}
         max={60}
         disabled={!!activeCycle}
         {...register('minutesAmount', {
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,7 +16,7 @@ const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmount: zod
     .number()
-    .min(1, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
 
